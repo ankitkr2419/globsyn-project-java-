de4040007ec3feab841f2652e1436bfcee942e3f
AnkitKumar <ankitkr1420@gmail.com>
2019-07-09 10:57:20 +0530

    initial commit

---
+++ globsyn_hw/ALEmpDemo/src/pack1/ALEmpmain.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class ALEmpmain {
+
+	public static void main(String[] args) {
+	ALEmpLogic obj= new ALEmpLogic();
+	obj.UserInput();
+	obj.display();
+	obj.update();
+	}
+}
